4ce77c83b5c1faa7bc82340dacc4b447434e2a7c
diliprathod32 <diliprathod32@gmail.com>
2021-02-02 12:51:03 +0530

    [Dilip] ClassMethodProgram

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMethod
+{
+    class Math
+    {
+
+        // Here Sum() method asks for two parameters from the user  
+        // calculates the sum of these  
+        // and finally returns the result. 
+        static int Sum(int x, int y)
+        {
+
+            // there are two local variables, local variable bcs we takes varibale in method  
+            int a = x;
+            int b = y;
+
+            // The local variable calculates the sum of 'a' and 'b' 
+            // and returns the result 
+            // which is of 'int' type. 
+            int result = a + b;
+
+            return result;
+        }
+
+        // Main Method 
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 23;
+
+            // Method Sum() is invoked and  
+            // the returned value is stored  
+            // in the local variable say 'c' 
+            int c = Sum(a, b);
+
+            // Display Result 
+            Console.WriteLine("The Value of the sum is " + c);
+        }
+    }
+}
